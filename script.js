161d4f01a1bd7430752caf4f69bb71828183b971
emuel-vassallo <emuelvassallo15@gmail.com>
2022-06-02 15:54:00 +0200

    add operate function

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+const operate = (operator, a, b) => {
+  return {
+    '+': add(a, b),
+    '-': subtract(a, b),
+    '*': multiply(a, b),
+    '/': divide(a, b),
+  }[operator];
+};
